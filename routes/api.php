306f9d8a7df23f06741cd1d595b53545daa9e9fa
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2023-06-13 00:05:56 +0200

    Test Route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get('ping', function () {
+    return response()->json([
+        'data' => 'pong',
+    ], Response::HTTP_OK);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();

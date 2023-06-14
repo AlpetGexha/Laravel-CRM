21859e60f18c04ad351f8f84eedfd875ee64aa9c
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2023-06-14 23:54:37 +0200

    Laravel Breeze with AUTHAPI

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,21 +1,7 @@
 <?php
 
-namespace Tests\Feature;
+test('example', function () {
+    $response = $this->get('/');
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @test
-     */
-    public function the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-}
+    $response->assertStatus(200);
+});
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,18 +1,5 @@
 <?php
 
-namespace Tests\Unit;
-
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @test
-     */
-    public function that_true_is_true(): void
-    {
-        $this->assertTrue(true);
-    }
-}
+test('example', function () {
+    expect(true)->toBeTrue();
+});

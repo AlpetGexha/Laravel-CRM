1d56dd81e1c2a106ddb4602f5dcdb509b5851a59
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2023-06-14 22:42:44 +0200

    Update ContactTest.php

--- tests/Feature/Api/V1/ContactTest.php
+++ tests/Feature/Api/V1/ContactTest.php
@@ -10,28 +10,25 @@ use function Pest\Laravel\actingAs;
 use function Pest\Laravel\getJson;
 use function Pest\Laravel\postJson;
 
-it('get Unauthorize respond if user its not login', function () {
-
+it('returns an unauthorized response if the user is not logged in', function () {
     $response = getJson(route('api.contact.index'));
-
     $response->assertStatus(Response::HTTP_UNAUTHORIZED);
 });
 
 it('can show a list of contacts for a user', function () {
-    auth()->loginUsingId(User::factory()->create()->id);
+    actingAs(User::factory()->create());
 
     Contact::factory(10)->create();
 
     $response = getJson(route('api.contact.index'));
 
-    $response->assertStatus(200)
-        ->assertJson(function (AssertableJson $json) {
-            $json->has('data', 10) // Assuming there are 10 contacts
-                ->whereAllType([
-                    'data' => 'array',
-                ])
-                ->etc(); // Add more assertions as needed for the specific structure of your data
-        });
+    $response->assertOk()
+        ->assertJson(fn (AssertableJson $json) => $json
+            ->has('data', 10)
+            ->whereAllType([
+                'data' => 'array',
+            ])
+            ->etc());
 });
 
 it('receives a 401 on create when not logged in', function (string $string) {
@@ -51,20 +48,13 @@ it('receives a 401 on create when not logged in', function (string $string) {
     $response->assertUnauthorized();
 })->with('strings');
 
-it('can create an new contact', function (string $string) {
+it('can create a new contact', function (string $string) {
+    actingAs(User::factory()->create());
 
-    auth()->loginUsingId(User::factory()->create()->id);
+    expect(Contact::count())->toEqual(0);
 
-    expect(Contact::query()->count())->toEqual(0);
     $data = [
         'title' => $string,
-        // 'name' => [
-        //     'first' => $string,
-        //     'middle' => $string,
-        //     'last' => $string,
-        //     'preferred' => $string,
-        //     'full' => "{$string} {$string} {$string}",
-        // ],
         'first_name' => $string,
         'middle_name' => $string,
         'last_name' => $string,
@@ -78,23 +68,19 @@ it('can create an new contact', function (string $string) {
 
     $response
         ->assertCreated()
-        ->assertJson(function (AssertableJson $json) use ($string) {
-            $json
-                ->where('data.name.first', $string)
-                ->where('data.name.middle', $string)
-                ->where('data.name.last', $string)
-                ->where('data.name.preferred', $string)
-                ->where('data.name.full', "{$string} {$string} {$string}")
-                ->etc();
-        });
+        ->assertJson(fn (AssertableJson $json) => $json
+            ->where('data.name.first', $string)
+            ->where('data.name.middle', $string)
+            ->where('data.name.last', $string)
+            ->where('data.name.preferred', $string)
+            ->where('data.name.full', "{$string} {$string} {$string}")
+            ->etc());
 
-    expect(Contact::query()->count())->toEqual(1);
+    expect(Contact::count())->toEqual(1);
 })->with('strings');
 
-
-it('can show a sigle contact', function () {
-
-    auth()->loginUsingId(User::factory()->create()->id);
+it('can show a single contact', function () {
+    actingAs(User::factory()->create());
 
     $contact = Contact::factory()->create();
 
@@ -103,24 +89,7 @@ it('can show a sigle contact', function () {
     $response->assertOk();
 });
 
-// it('can update the contact', function () {
-
-//     auth()->loginUsingId(User::factory()->create()->id);
-
-//     $contact = Contact::factory()->create();
-
-//     $string = 'Alpet-2';
-
-//     $data = [
-//         'title' => $string,
-//         'first_name' => $string,
-//         'middle_name' => $string,
-//         'last_name' => $string,
-//         'preferred_name' => $string,
-//         'email' => $string . '@gmail.com',
-//         'phone' => $string,
-//         'pronouns' => PronounsEnum::random(),
-//     ];
+it('user can update the contact', function () {
     $contact = Contact::factory()->create();
 
     $data = [
@@ -128,7 +97,7 @@ it('can show a sigle contact', function () {
         'first_name' => fake()->name,
         'middle_name' => fake()->name,
         'last_name' => fake()->name,
-        'preferred_name' => fake()->name . " GG",
+        'preferred_name' => fake()->name . ' GG',
         'email' => fake()->email,
         'phone' => fake()->phoneNumber,
         'pronouns' => PronounsEnum::random(),

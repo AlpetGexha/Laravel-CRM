9ce9e1856c4e6e4bee816c19d952f959976c3167
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2023-06-14 04:02:51 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('ping', function () {
     return response()->json([
-        'data' => 'pong',
+        'ack' => 'pong',
     ], Response::HTTP_OK);
 });
 

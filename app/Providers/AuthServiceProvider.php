5027ef21b21f465372ade00e650fcb26be517ec1
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2023-06-16 02:22:01 +0200

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,15 @@
 
 namespace App\Providers;
 
+use App\Models\Company;
+use App\Models\Contact;
+use App\Models\Interaction;
+use App\Models\JobTitle;
+use App\Policies\CompanyPolicy;
+use App\Policies\ContactPolicy;
+use App\Policies\InteractionPolicy;
+use App\Policies\JobTitlePolicy;
+use App\Policies\UserPolicy;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -13,7 +22,11 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        User::class => UserPolicy::class,
+        Contact::class => ContactPolicy::class,
+        Interaction::class => InteractionPolicy::class,
+        Company::class => CompanyPolicy::class,
+        JobTitle::class => JobTitlePolicy::class,
     ];
 
     /**
@@ -24,7 +37,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url') . "/password-reset/{$token}?email={$notifiable->getEmailForPasswordReset()}";
+            return config('app.frontend_url')."/password-reset/{$token}?email={$notifiable->getEmailForPasswordReset()}";
         });
 
         //
